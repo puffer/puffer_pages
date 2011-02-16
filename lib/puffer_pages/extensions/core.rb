2939fc4416c72419c6c2fa9734888a41adc2a5a2
pyromaniac <kinwizard@gmail.com>
2011-02-16 10:56:28 +0300

    Added arranged method to array

--- lib/puffer_pages/extensions/core.rb
+++ lib/puffer_pages/extensions/core.rb
@@ -5,3 +5,18 @@ Kernel.class_eval do
     nil
   end
 end
+
+Array.class_eval do
+  def arrange
+    arranged = ActiveSupport::OrderedHash.new
+    insertion_points = [arranged]
+    depth = 0
+    each do |node|
+      insertion_points.push insertion_points.last.values.last if node.depth > depth
+      (depth - node.depth).times { insertion_points.pop } if node.depth < depth
+      insertion_points.last.merge! node => ActiveSupport::OrderedHash.new
+      depth = node.depth
+    end
+    arranged
+  end
+end
