c90047c68b18223cc028e99842e9b56ea583cf19
pyromaniac <kinwizard@gmail.com>
2012-05-01 19:41:04 +0700

    Main controller handles path_info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   unloadable
 
   def index
-    page = Page.find_page(params[:path])
+    page = Page.find_page(request.path_info)
     render :puffer_page => page, :content_type => page.content_type
   end
 end
