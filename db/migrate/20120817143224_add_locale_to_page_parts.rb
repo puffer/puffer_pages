198ea44484b00941eb89955c823b5dfca5a996f2
pyromaniac <kinwizard@gmail.com>
2012-09-19 21:04:41 +0700

    Ups

--- db/migrate/20120817143224_add_locale_to_page_parts.rb
+++ db/migrate/20120817143224_add_locale_to_page_parts.rb
@@ -1,7 +1,7 @@
 class AddLocaleToPageParts < ActiveRecord::Migration
   def self.up
     add_column :page_parts, :locale, :string
-    Page.update_all :locale => I18.default_locale
+    Page.update_all :locale => I18n.default_locale
   end
 
   def self.down
