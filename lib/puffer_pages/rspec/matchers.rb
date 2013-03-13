858193ae9fe601564073f7863157e6232182ac52
exoth <yuri.trofimenko@aviasales.ru>
2013-03-13 18:43:04 +0700

    Fixed  rspec matchers.

--- lib/puffer_pages/rspec/matchers.rb
+++ lib/puffer_pages/rspec/matchers.rb
@@ -13,15 +13,18 @@ RSpec::Rails::ControllerExampleGroup.class_eval do
     @puffer_pages_render
   end
 
-  included do
-    around(:each) do |example|
-      @puffer_pages_render = {}
-      ActiveSupport::Notifications.subscribe('render_page.puffer_pages') do |name, start, finish, id, payload|
-        @puffer_pages_render[payload[:subject]] ||= []
-        @puffer_pages_render[payload[:subject]].push payload
+  def self.included base
+    base.class_eval do
+      around(:each) do |example|
+        @puffer_pages_render = {}
+        ActiveSupport::Notifications.subscribe('render_page.puffer_pages') do |name, start, finish, id, payload|
+          @puffer_pages_render[payload[:subject]] ||= []
+          @puffer_pages_render[payload[:subject]].push payload
+        end
+        example.run
+        ActiveSupport::Notifications.unsubscribe('render_page.puffer_pages')
       end
-      example.run
-      ActiveSupport::Notifications.unsubscribe('render_page.puffer_pages')
     end
+    super
   end
 end
