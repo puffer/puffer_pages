8b4fa27da2159ec0bf6fbd209cc8f146b7f7c7a5
Exoth <yuri.trofimenko@aviasales.ru>
2014-12-05 13:14:41 +0700

    Add additional logging.

--- lib/puffer_pages/log_subscriber.rb
+++ lib/puffer_pages/log_subscriber.rb
@@ -2,22 +2,33 @@ module PufferPages
   class LogSubscriber < ActiveSupport::LogSubscriber
     def render_page event
       message = "  PufferPages: rendered page /#{event.payload[:subject].location} #{duration(event)}"
-      info message
+      persistent_log event.payload[:subject].location, event.duration
+      debug message
     end
 
     def render_page_part event
       message = "  PufferPages: rendered page_part #{event.payload[:subject].name} #{duration(event)}"
+      persistent_log event.payload[:subject].name, event.duration
       debug message
     end
 
     def render_layout event
       message = "  PufferPages: rendered layout #{event.payload[:subject].name} #{duration(event)}"
+      persistent_log event.payload[:subject].name, event.duration
       debug message
     end
 
     def render_snippet event
       message = "  PufferPages: rendered snippet #{event.payload[:subject].name} #{duration(event)}"
+      persistent_log event.payload[:subject].name, event.duration
       debug message
+    end
+
+    def persistent_log id, duration
+      if Rails.env != 'development'
+        message = "Template debug - id: #{CmsEngine::DomainConfig.current.locale}_#{id} time: #{duration}"
+        error message
+      end
     end
 
     def duration event
